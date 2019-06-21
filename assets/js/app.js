aa5525e5a843c740ce35534ba0ffc9b1f5007bf1
Bankole Esan <techybanky@gmail.com>
2019-06-21 17:33:30 +0100

    added moment.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,20 @@
 const closeAll = () => {
-  $("#androidcourses").slideToggle();
-  $("#cloudcourses").slideToggle();
-  $("#mobilewebcourses").slideToggle();
+  $("#androidchannels").slideToggle();
+  $("#cloudchannels").slideToggle();
+  $("#mobilewebchannels").slideToggle();
+  $(".course").slideToggle();
 };
 
 closeAll();
 
-$("#viewandchannels").click(function(e) {
+$(".viewchannel").click(function(e) {
   e.preventDefault();
-  $("#androidcourses").slideToggle("300");
+  target = e.target.id;
+  $(`#${target}channels`).slideToggle("300");
 });
 
-$("#viewcloudchannels").click(function(e) {
+$(".viewcourse").click(function(e) {
   e.preventDefault();
-  $("#cloudcourses").slideToggle("300");
-});
-
-$("#viewmobilewebchannels").click(function(e) {
-  e.preventDefault();
-  $("#mobilewebcourses").slideToggle("300");
+  target = e.target.id;
+  $(`#${target}courses`).slideToggle("300");
 });
