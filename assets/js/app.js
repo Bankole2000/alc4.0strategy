816ff9ae48e5c3fe60671c7b5aa2229d685e0656
Bankole Esan <techybanky@gmail.com>
2019-06-24 02:46:11 +0100

    Added Local Storage

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,6 +7,41 @@ const closeAll = () => {
   $(".course").slideToggle();
 };
 
+const colorChange = (status, el) => {
+  if (status === "done") {
+    el.classList.add("bg-success", "text-white");
+  } else if (status === "Not yet Done") {
+    el.classList.remove("bg-success", "text-white");
+  }
+};
+
+const addDone = id => {
+  let done; // INITIALIZE DONE ARRAY
+  if (localStorage.getItem("done") === null) {
+    // IF LOCAL STORAGE EMPTY
+    done = [];
+  } else {
+    done = JSON.parse(localStorage.getItem("done"));
+  }
+  done.push(id); // ADD TO DONE ARRAY
+  localStorage.setItem("done", JSON.stringify(done)); // STORE IN LOCAL STORAGE
+};
+
+const removeDone = id => {
+  let done;
+  if (localStorage.getItem("done") === null) {
+    done = [];
+  } else {
+    done = JSON.parse(localStorage.getItem("done"));
+  }
+  done.forEach((item, index) => {
+    if (item === id) {
+      done.splice(index, 1);
+    }
+  });
+  localStorage.setItem("done", JSON.stringify(done));
+};
+
 closeAll();
 
 $(".viewchannel").click(function(e) {
@@ -22,16 +57,49 @@ $(".viewcourse").click(function(e) {
 });
 
 $(window).click(function(e) {
-  let id, status, message;
+  let id, status, message, row;
   if (e.target.className.includes("form-check-input")) {
     id = e.target.id;
+    row = e.target.parentElement.parentElement.parentElement;
     if (e.target.checked) {
       status = "done";
+      // Add to Local Storage
+      addDone(id);
     } else {
       status = "Not yet Done";
+      // Remove from Local Storage
+      removeDone(id);
     }
+    colorChange(status, row);
     message = `${id} is ${status}`;
     M.toast({ html: message });
     console.log(`${id} is ${status}`);
   }
 });
+
+const getDone = () => {
+  let done;
+  if (localStorage.getItem("done") === null) {
+    done = [];
+  } else {
+    done = JSON.parse(localStorage.getItem("done"));
+  }
+  done.forEach(item => {
+    console.log(item);
+    let el = document.getElementById(item);
+    console.log(typeof item);
+    console.log(el);
+    let status = "done";
+    let row = el.parentElement.parentElement.parentElement;
+    document.getElementById(item).checked = true;
+    colorChange(status, row);
+  });
+};
+
+const getAllDone = () => {
+  setTimeout(() => {
+    getDone();
+  }, 2000);
+};
+
+document.addEventListener("DOMContentLoaded", getAllDone);
